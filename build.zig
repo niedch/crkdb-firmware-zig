0021e90e0f7bd0cae655d78d3471f78a3c55a375
nic <christoph.niederer99@gmail.com>
2026-03-09 19:16:26 +0100

    feat: Multibuild

--- build.zig
+++ build.zig
@@ -9,14 +9,16 @@ pub fn build(b: *std.Build) void {
     const mz_dep = b.dependency("microzig", .{});
     const mb = MicroBuild.init(b, mz_dep) orelse return;
 
-    const firmware = mb.add_firmware(.{
-        .name = "blinky",
-        .target = mb.ports.rp2xxx.boards.raspberrypi.pico,
-        .optimize = .ReleaseSmall,
-        .root_source_file = b.path("src/main.zig"),
-    });
+    for ([_][]const u8{ "left", "right" }) |unit| {
+        const firmware = mb.add_firmware(.{
+            .name = b.fmt("crkdb_firmware-{s}", .{unit}),
+            .target = mb.ports.rp2xxx.boards.raspberrypi.pico,
+            .optimize = .ReleaseSmall,
+            .root_source_file = b.path("src/main.zig"),
+        });
 
-    // We call this twice to demonstrate that the default binary output for
-    // RP2040 is UF2, but we can also output other formats easily
-    mb.install_firmware(firmware, .{});
+        // We call this twice to demonstrate that the default binary output for
+        // RP2040 is UF2, but we can also output other formats easily
+        mb.install_firmware(firmware, .{});
+    }
 }
